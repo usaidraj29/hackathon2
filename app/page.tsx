ed8505d214b484ff28db809b9db4678bdeea7609
usaidraj29 <usaidbabarkapankha29@gmail.com>
2024-12-09 01:32:55 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import Hero from "./components/hero";
+import Brand from "./components/brand";
+import Ceramics from "./components/ceramics";
+import Product from "./components/product";
+import Benefit from "./components/Benifit";
+import Touch from "./components/touch";
+import ProductListing from "./productlistings/page";
+
+
+export default function Home() {
+  return (
+   <>
+   <Hero />
+   <Brand />
+   <Ceramics />
+   <Product />
+   <Benefit />
+   <Touch />
+<ProductListing/>
+   
+
+   </>
+  );
+}
